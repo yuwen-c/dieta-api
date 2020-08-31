e06614d882d9c902df958546b1b13cb02a4ae742
marine <marine@MarinedeiMac.local>
2020-08-31 15:44:42 +0800

    build a server, make a plan

---
+++ server.js
@@ -0,0 +1,13 @@
+
+
+
+// plan:
+// "/" get: to see if it's working
+// "/signin" post: sign in 
+// "/register" post: register
+// "/loadActivity" get: get activity data last week from database
+// "/loadExercise" get: get exercise data
+// "/calculate" get: 0. if weight = 0，從資料庫叫weight, deficit。1. do calculation, 2. show it on page (前端)
+//     3. save 所有數據 to database(weight, totalDeficit, 每日總熱量, 每日碳水量, 活動運動量, )
+// "/result" get: 從database叫出上次儲存的結果
+

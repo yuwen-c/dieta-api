c1c8b2ad269bd76262eb034eb2f525c35db3734e
marine <marine@MarinedeiMac.local>
2020-09-01 14:26:20 +0800

    connect postman to server

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const expresss = require('express');
+const express = require('express'); // import module
 
-const app = express();
+const app = express(); // create one
+
+// plan:
+// ok "/" get: to see if it's working
+// "/signin" post: sign in 
+// "/register" post: register
+// "/loadActivity" get: get activity data last week from database
+// "/loadExercise" get: get exercise data
+// "/calculate" get: 0. if weight = 0，從資料庫叫weight, deficit。1. do calculation, 2. show it on page (前端)
+//     3. save 所有數據 to database(weight, totalDeficit, 每日總熱量, 每日碳水量, 活動運動量, )
+// "/result" get: 從database叫出上次儲存的結果
+
+app.get('/', (req, res) => {
+    res.json("hi there!")
+})
+
+
 
 // create an object variable to mimic database
 const database = {
@@ -13,45 +29,45 @@ const database = {
         },
     ]
 } 
+
+// app.post('/signin', (req, res) => {
+//     console.log(req.email, req.password)
+//     if(req.email === 'marina@gmail.com' && req.password ==='1111'){
+//         res.json(database.users[0])
+//     }    else{
+//     res.status(404).json('wrong credential')}
+// })
 // 待修改
-const initialState = {
-    name: '',
-    email: '',
-    password: '',
+// const initialState = {
+//     name: '',
+//     email: '',
+//     password: '',
   
-    weight : 0,
-    BMR : 0,
-    isSignIn : false,
-    route: 'home', // sign in, sign up, weight, activity, exercise, nutrition
+//     weight : 0,
+//     BMR : 0,
+//     isSignIn : false,
+//     route: 'home', // sign in, sign up, weight, activity, exercise, nutrition
   
-    deficit : 0,
-    activity : [], // store week activity, like: ['0', '1', '0', '1', '0', '3', '2']
-    exercise : [], // store week exercise, like: ['0', '1', '0', '1', '0', '3', '2']
+//     deficit : 0,
+//     activity : [], // store week activity, like: ['0', '1', '0', '1', '0', '3', '2']
+//     exercise : [], // store week exercise, like: ['0', '1', '0', '1', '0', '3', '2']
     
-    protein : 0,
-    oil : 0,
-    dailyCalorie : [], // 7 days daily calorie
-    dailyCarbon : [], // 7 days daily carbohydrate
+//     protein : 0,
+//     oil : 0,
+//     dailyCalorie : [], // 7 days daily calorie
+//     dailyCarbon : [], // 7 days daily carbohydrate
   
-    checkedActivity : initialchecked,
-    checkedExercise : initialchecked,
-    // the default of checked attribute of options
+//     checkedActivity : initialchecked,
+//     checkedExercise : initialchecked,
+//     // the default of checked attribute of options
   
-    modifySpeedUp: false,
-    modifySlowDown: false, 
-    modifyOption: 0,
-  }
+//     modifySpeedUp: false,
+//     modifySlowDown: false, 
+//     modifyOption: 0,
+//   }
   
 
 
-// plan:
-// "/" get: to see if it's working
-// "/signin" post: sign in 
-// "/register" post: register
-// "/loadActivity" get: get activity data last week from database
-// "/loadExercise" get: get exercise data
-// "/calculate" get: 0. if weight = 0，從資料庫叫weight, deficit。1. do calculation, 2. show it on page (前端)
-//     3. save 所有數據 to database(weight, totalDeficit, 每日總熱量, 每日碳水量, 活動運動量, )
-// "/result" get: 從database叫出上次儲存的結果
 
-app.listen(3000);
+// set port
+app.listen(3000, console.log("server is running on 3000!"));

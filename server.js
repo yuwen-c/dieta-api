8bdd17ebd55fabd2974a38b9a23f37eb34b6fb1c
yuwenGithub <yuwen.azulejos@gmail.com>
2020-10-13 16:39:22 +0800

    comment out the big database obj. rewrite result endpoint, version 1, get calorie and carbon done

--- server.js
+++ server.js
@@ -34,76 +34,76 @@ app.get('/', (req, res) => {
 // users => table [ ] = 想成都是object, 只是這個table需要有名字，所以用
 // users[0] => table裡面一筆資料 { }
 // users[0] 裡面的pair: { }的property 是欄位名稱，value是資料
-const database = {
-    table_userLogin: [
-        {
-            id:123,
-            email: 'marina@gmail.com',
-            password: '1111',
-        },
-    ],
-    table_users:[
-        {
-            name:'marina',
-            email: 'marina@gmail.com',
-            weight: 55,
-            deficit: 300,
-        }
-    ],
-    table_activity: [
-        {
-            email:'marina@gmail.com',  //table column改為email
-            0:'1',  // table column 改為day1, day2...
-            1:'1',
-            2:'1',
-            3:'1',
-            4:'0',
-            5:'3',
-            6:'2',
-        }
-    ],
-    table_exercise: [
-        {
-            email:'marina@gmail.com',
-            0:'0',
-            1:'0',
-            2:'0',
-            3:'2',
-            4:'0',
-            5:'2',
-            6:'1',
-        }
-    ],
-    table_carbohydrate: [
-        {
-            email: 'marina@gmail.com',
-            day1:84,
-            day2:84,
-            day3:84,
-            day4:145,
-            day5:54,
-            day6:205,
-            day7:145,
+// const database = {
+//     table_userLogin: [
+//         {
+//             id:123,
+//             email: 'marina@gmail.com',
+//             password: '1111',
+//         },
+//     ],
+//     table_users:[
+//         {
+//             name:'marina',
+//             email: 'marina@gmail.com',
+//             weight: 55,
+    //         deficit: 300,
+    //     }
+    // ],
+    // table_activity: [
+    //     {
+    //         email:'marina@gmail.com',  //table column改為email
+    //         0:'1',  // table column 改為day1, day2...
+    //         1:'1',
+    //         2:'1',
+    //         3:'1',
+    //         4:'0',
+    //         5:'3',
+    //         6:'2',
+    //     }
+    // ],
+    // table_exercise: [
+    //     {
+    //         email:'marina@gmail.com',
+    //         0:'0',
+    //         1:'0',
+    //         2:'0',
+    //         3:'2',
+    //         4:'0',
+    //         5:'2',
+    //         6:'1',
+    //     }
+    // ],
+    // table_carbohydrate: [
+    //     {
+    //         email: 'marina@gmail.com',
+    //         day1:84,
+    //         day2:84,
+    //         day3:84,
+    //         day4:145,
+    //         day5:54,
+    //         day6:205,
+    //         day7:145,
 
-        }
-    ],
-    table_totalCalorie: [
-        {
-            email: 'marina@gmail.com',
-            // protein: 0,
-            // oil:0,
+    //     }
+    // ],
+//     table_totalCalorie: [
+//         {
+//             email: 'marina@gmail.com',
+//             // protein: 0,
+//             // oil:0,
 
-            day1:1273,
-            day2:1273,
-            day3:1273,
-            day4:1515,
-            day5:1152,
-            day6:1757,
-            day7:1515,
-        }
-    ]
+//             day1:1273,
+//             day2:1273,
+//             day3:1273,
+//             day4:1515,
+//             day5:1152,
+//             day6:1757,
+//             day7:1515,
+//         }
+//     ]
 
-} 
+// } 
 
 // compare password and return user data to front end
 app.post('/signin', (req, res) => {
@@ -304,12 +304,24 @@ app.put("/calculate", (req, res) => {
 
 // "/result" : post 從database叫出上次儲存的結果
 app.post("/result", (req, res) => {
-    if(req.body.email === database.table_totalCalorie[0].userEmail){
-        res.json({
-            dailyCalorie: database.table_totalCalorie[0],
-            dailyCarbon: database.table_carbohydrate[0]
+    const {email} = req.body;
+
+    db("carbohydrate")
+    .where({email: email})
+    .select("*")
+    .then(userCarbon => {
+        db("totalcalorie")
+        .where({email: email})
+        .select("*")
+        .then(userCalorie => {
+            res.json({
+                userCarbon: userCarbon[0],
+                userCalorie: userCalorie[0]
+            })
         })
-    }
+        .catch(error => console.log(error))
+    })
+    .catch(error => console.log(error));
 })
 
 

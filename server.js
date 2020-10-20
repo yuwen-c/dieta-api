4b0ac95c543bd610132461a3799c05fe2b179a04
yuwenGithub <yuwen.azulejos@gmail.com>
2020-10-20 16:57:37 +0800

    add check in every endpoint and catch to every query

--- server.js
+++ server.js
@@ -21,11 +21,10 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 
-// console.log("db", db.select('*').from('users'));
-
 app.get('/', (req, res) => {
     db.select('*').from('users')
     .then(result => res.json(result))
+    .catch(console.log)
     // res.json("hi there!")
 })
 
@@ -121,6 +120,7 @@ app.post('/signin', (req, res) => {
                         email: user[0].email
                     })
                     .then(userData => {res.json(userData[0])})
+                    .catch(console.log)
                 }
                 else{
                     res.json("Sign in failure.")
@@ -130,14 +130,23 @@ app.post('/signin', (req, res) => {
         })
         .catch(console.log)
     }
+    else{
+        res.json("Sign in failure.");
+    }
 })
 
 // get user data
 app.post('/getUser', (req, res) => {
-    db('users')
-    .where({email: req.body.email})
-    .then(user => {res.json(user[0])})
-    .catch(console.log);
+    const {email} = req.body.email;
+    if(email){
+        db('users')
+        .where({email: email})
+        .then(user => {res.json(user[0])})
+        .catch(console.log);        
+    }
+    else{
+        res.json("Unable to get user");
+    }
 })
 
 // use transaction to add one data to two tables: userlogin, users
@@ -165,181 +174,193 @@ app.post('/signup', (req, res) => {
         })
         .catch(error => console.log(error));
     }
+    else{
+        res.json("Sign up failure.");
+    }
 })
 
 // load activity record
 app.post("/activity", (req, res) => {
     const {email} = req.body;
-
-    db("users")
-    .where({email: email})
-    .select("weight")
-    .then(weight => {
-    // if the user have never saved data, the weight will be 0.
-        if(weight[0].weight === 0){
-            res.json("No saved record! please choose the options.")
-        }
-        else{
-            db("activity")
-            .where({email: email})
-            .select("*")
-            .then(userActivity => {
-                res.json(userActivity[0])
-            })
-            .catch(console.log);
-        }
-    })
-    .catch(console.log);
+    if(email){
+        db("users")
+        .where({email: email})
+        .select("weight")
+        .then(weight => {
+        // if the user have never saved data, the weight will be 0.
+            if(weight[0].weight === 0){
+                res.json("No saved record! please choose the options.")
+            }
+            else{
+                db("activity")
+                .where({email: email})
+                .select("*")
+                .then(userActivity => {
+                    res.json(userActivity[0])
+                })
+                .catch(console.log);
+            }        
+        })
+        .catch(console.log);        
+    }
+    else{
+        res.json("Get activity record failure.");
+    }
 })
 
 // load exercise record
 app.post("/exercise", (req, res) => {
     const {email} = req.body;
-
-    db("users")
-    .where({email: email})
-    .select("weight")
-    .then(weight => {
-    // if the user have never saved data, the weight will be 0.
-        if(weight[0].weight === 0){
-            res.json("No saved record! please choose the options.")
-        }
-        else{
-            db("exercise")
-            .where({email: email})
-            .select("*")
-            .then(userExercise => {
-                res.json(userExercise[0])
-            })
-            .catch(console.log);
-        }
-    })
-    .catch(console.log);
+    if(email){
+        db("users")
+        .where({email: email})
+        .select("weight")
+        .then(weight => {
+        // if the user have never saved data, the weight will be 0.
+            if(weight[0].weight === 0){
+                res.json("No saved record! please choose the options.")
+            }
+            else{
+                db("exercise")
+                .where({email: email})
+                .select("*")
+                .then(userExercise => {
+                    res.json(userExercise[0])
+                })
+                .catch(console.log);
+            }
+        })
+        .catch(console.log);        
+    }
+    else{
+        res.json("Get exercise record failure.");
+    }
 })
 
 // save data to tables: weight, deficit, activity, exercise, carbohydrate, totalcalorie
 app.put("/calculate", (req, res) => {
     const {email, weight, deficit, activity, exercise, dailyCarbon, dailyCalorie} = req.body;
-    console.log(req.body);
-
-    db.transaction(trx => {
-        return trx('users')
-        .where({email: email})
-        .update({
-            weight: weight,
-            deficit: deficit
-        })
-        .then(() => {
-            return trx('activity')
+    if(email, weight, deficit, activity, exercise, dailyCarbon, dailyCalorie){
+        db.transaction(trx => {
+            return trx('users')
             .where({email: email})
             .update({
-                day1: activity[0],
-                day2: activity[1],
-                day3: activity[2],
-                day4: activity[3],
-                day5: activity[4],
-                day6: activity[5],
-                day7: activity[6],
+                weight: weight,
+                deficit: deficit
             })
             .then(() => {
-                return trx('exercise')
+                return trx('activity')
                 .where({email: email})
                 .update({
-                    day1: exercise[0],
-                    day2: exercise[1],
-                    day3: exercise[2],
-                    day4: exercise[3],
-                    day5: exercise[4],
-                    day6: exercise[5],
-                    day7: exercise[6]
+                    day1: activity[0],
+                    day2: activity[1],
+                    day3: activity[2],
+                    day4: activity[3],
+                    day5: activity[4],
+                    day6: activity[5],
+                    day7: activity[6],
                 })
                 .then(() => {
-                    return trx("carbohydrate")
+                    return trx('exercise')
                     .where({email: email})
                     .update({
-                        day1: dailyCarbon[0],
-                        day2: dailyCarbon[1],
-                        day3: dailyCarbon[2],
-                        day4: dailyCarbon[3],
-                        day5: dailyCarbon[4],
-                        day6: dailyCarbon[5],
-                        day7: dailyCarbon[6]
+                        day1: exercise[0],
+                        day2: exercise[1],
+                        day3: exercise[2],
+                        day4: exercise[3],
+                        day5: exercise[4],
+                        day6: exercise[5],
+                        day7: exercise[6]
                     })
                     .then(() => {
-                        return trx("totalcalorie")
+                        return trx("carbohydrate")
                         .where({email: email})
                         .update({
-                            day1: dailyCalorie[0],
-                            day2: dailyCalorie[1],
-                            day3: dailyCalorie[2],
-                            day4: dailyCalorie[3],
-                            day5: dailyCalorie[4],
-                            day6: dailyCalorie[5],
-                            day7: dailyCalorie[6]
+                            day1: dailyCarbon[0],
+                            day2: dailyCarbon[1],
+                            day3: dailyCarbon[2],
+                            day4: dailyCarbon[3],
+                            day5: dailyCarbon[4],
+                            day6: dailyCarbon[5],
+                            day7: dailyCarbon[6]
+                        })
+                        .then(() => {
+                            return trx("totalcalorie")
+                            .where({email: email})
+                            .update({
+                                day1: dailyCalorie[0],
+                                day2: dailyCalorie[1],
+                                day3: dailyCalorie[2],
+                                day4: dailyCalorie[3],
+                                day5: dailyCalorie[4],
+                                day6: dailyCalorie[5],
+                                day7: dailyCalorie[6]
+                            })
                         })
                     })
                 })
             })
         })
-    })
-    .then(() => res.json("ok"))
-    .catch(error => console.log(error));
+        .then(() => res.json("ok"))
+        .catch(error => console.log(error));
+    }
+    else{
+        res.json("Calculation failure.");
+    }
 })
 
 // "/result" : post 從database叫出上次儲存的結果
 app.post("/result", (req, res) => {
     const {email} = req.body;
-    db("users")
-    .where({email: email})
-    .select("*")
-    .then(user => {
-        db("carbohydrate")
+    if(email){
+        db("users")
         .where({email: email})
         .select("*")
-        .then(userCarbon => {
-            db("totalcalorie")
+        .then(user => {
+            db("carbohydrate")
             .where({email: email})
             .select("*")
-            .then(userCalorie => {
-                db("activity")
+            .then(userCarbon => {
+                db("totalcalorie")
                 .where({email: email})
                 .select("*")
-                .then(userActivity => {
-                    db("exercise")
+                .then(userCalorie => {
+                    db("activity")
                     .where({email: email})
                     .select("*")
-                    .then(userExercise => {
-                        res.json({
-                            user: user[0],
-                            userCarbon: userCarbon[0],
-                            userCalorie: userCalorie[0],
-                            userActivity: userActivity[0],
-                            userExercise: userExercise[0]
+                    .then(userActivity => {
+                        db("exercise")
+                        .where({email: email})
+                        .select("*")
+                        .then(userExercise => {
+                            res.json({
+                                user: user[0],
+                                userCarbon: userCarbon[0],
+                                userCalorie: userCalorie[0],
+                                userActivity: userActivity[0],
+                                userExercise: userExercise[0]
+                            })
                         })
+                        .catch(console.log);
                     })
+                    .catch(console.log);
                 })
+                .catch(console.log);
             })
+            .catch(console.log);
         })
-        .catch(error => console.log(error))
-    })
-    .catch(error => console.log(error));
+        .catch(console.log);
+    }
+    else{
+        res.json("Get resule failure.")
+    }
 })
 
 
 // set port
 // app.listen(3000, () => {console.log("server is running on 3000!")});
 
-// make a port for heroku
+// port for heroku
 app.listen(process.env.PORT || 3000, () => {
     console.log(`it's running on PORT ${process.env.PORT}`);
 })
-
-// plan:
-// ok "/" get: to see if it's working
-// ok "/signin" post: sign in, return user 
-// ok "/register" post: register
-// ok"/activity" post: get activity data last week from database
-// ok"/exercise" post: get exercise data
-// ok"/calculate" put: 0. if weight = 0，從資料庫叫weight, deficit。1. do calculation, 2. show it on page (前端)
-//     3. save 所有數據 to database(weight, totalDeficit, 每日總熱量, 每日碳水量, 活動運動量, )
-// ok"/result" post: 從database叫出上次儲存的結果

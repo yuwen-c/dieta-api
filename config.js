f3316cc0ba371fae25155d2d651d2452890e6410
yuwen <yuwen.azulejos@gmail.com>
2023-03-20 18:05:58 +0800

    chore: install dotenv, set config.js, add knex connection configuration and save secrets in env

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: process.env.PORT,
+  KNEX_HOST: process.env.KNEX_HOST,
+  KNEX_DATABASE: process.env.KNEX_DATABASE,
+  KNEX_USER: process.env.KNEX_USER,
+  KNEX_PASSWORD: process.env.KNEX_PASSWORD,
+};
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 const cors = require('cors');
 const knex = require('knex');
 const bcrypt = require('bcrypt-nodejs');
@@ -9,31 +9,52 @@ const activity = require('./controllers/activity');
 const exercise = require('./controllers/exercise');
 const saveData = require('./controllers/saveData');
 const result = require('./controllers/result');
+const {
+  port,
+  KNEX_HOST,
+  KNEX_DATABASE,
+  KNEX_USER,
+  KNEX_PASSWORD,
+} = require('./config');
 
 const app = express(); // create one
 
+// connect heroku server to heroku database
 const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
-        }
+  client: 'pg',
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
     //   host : '127.0.0.1',
     //   user : '',
     //   password : '',
     //   database : 'Dieta'
-    }
-  });
+  },
+});
+
+// connect local server to heroku database
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     host: KNEX_HOST,
+//     user: KNEX_USER,
+//     password: KNEX_PASSWORD,
+//     database: KNEX_DATABASE,
+//     ssl: {
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
 app.use(cors());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-
 app.get('/', (req, res) => {
-    res.json("hi there!")
-})
+  res.json('hi there!');
+});
 
 // get user data
 // app.post('/user', (req, res) => {   })  //original syntax
@@ -42,28 +63,26 @@ app.post('/user', (req, res) => user.handleUser(req, res, db));
 // compare password and return user data to front end
 app.post('/signin', (req, res) => signin.handleSignin(req, res, db, bcrypt));
 
-
 // use transaction to add one data to two tables: userlogin, users
 // also, create a user in every table with default value 0:
 app.post('/signup', (req, res) => signup.handleSignup(req, res, db, bcrypt));
 
 // load activity record
-app.post("/activity", (req, res) => activity.handleActivity(req, res, db));
+app.post('/activity', (req, res) => activity.handleActivity(req, res, db));
 
 // load exercise record
-app.post("/exercise", (req, res) => exercise.handleExercise(req, res, db));
+app.post('/exercise', (req, res) => exercise.handleExercise(req, res, db));
 
 // save data to tables: weight, deficit, activity, exercise, carbohydrate, totalcalorie
-app.put("/saveData", (req, res) => saveData.handleSaveData(req, res, db));
+app.put('/saveData', (req, res) => saveData.handleSaveData(req, res, db));
 
 // "/result" : get the latest calculation result
-app.post("/result", (req, res) => result.handleResult(req, res, db));
-
+app.post('/result', (req, res) => result.handleResult(req, res, db));
 
 // set port
 // app.listen(3000, () => {console.log("server is running on 3000!")});
 
 // port for heroku
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`it's running on PORT ${process.env.PORT} `);
-})
+app.listen(port || 3000, () => {
+  console.log(`it's running on PORT ${port} `);
+});
